2c2417d9fbe1f01a02e61199c16529bdc3547d95
fernando <fernando.montero@osatech.ch>
2024-03-28 15:57:34 +0100

    Added Barner PWA

--- src/app/UI/Pwamodal.tsx
+++ src/app/UI/Pwamodal.tsx
@@ -8,7 +8,7 @@ export default function Pwamodal({show, onClose, onInstall}: any) {
     show &&(
       <div className=" fixed bottom-0 flex items-center justify-center z-50">
           <div className="bg-white w-94 p-4 roundend-lg shadow-lg">
-              <h2 className="text-lg font-semibold mb-2 text-black"> Installa l'App</h2>
+              <h2 className="text-lg font-semibold mb-2 text-black"> Installa App</h2>
               <p className="text-sm mb-4 text-black">Clicca installa per salvare questa applicazione sul tuo cellulare.</p>
               <div className="flex">
                 <button onClick={onClose} className='bg-gray-300 hover:bg-gray-400 text-gray-700 px-4 py-2 rounded-lg mr-4'>Chiudi</button>
