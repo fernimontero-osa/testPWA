766c50adbef5064605083af985468f661cb279b8
fernando <fernando.montero@osatech.ch>
2024-03-28 13:04:11 +0100

    Added File

---
+++ src/app/UI/Sidebar/Sidebar.tsx
@@ -0,0 +1,10 @@
+
+export default function Sidebar() {
+  return (
+    <div className="border-1 border-gray-200 w-80 mr-20 shadow-xl bg-white md:block hidden">
+        <div className="flex flex-col p-10">
+            <h4 className="font-bold w-full border-2 rounded-lg p-1 border-slate-500 hover:bg-slate-500 hover:color-white hover:cursor-pointer">Home</h4>
+        </div>
+    </div>
+  )
+}
